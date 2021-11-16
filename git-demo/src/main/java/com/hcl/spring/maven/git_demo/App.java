c364fa52761057b31d9ae18970f18493fd5cc2c7
Saikumar1234 <menda.saikumar@hcl.com>
2021-11-16 16:15:35 +0530

    first commit

---
+++ git-demo/src/main/java/com/hcl/spring/maven/git_demo/App.java
@@ -0,0 +1,13 @@
+package com.hcl.spring.maven.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
